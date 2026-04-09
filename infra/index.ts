b9d13a52ef77ce6ccb0aee24fed2a20b0e4506c4
Guilherme Rigotti <ggrigotti@gmail.com>
2026-04-09 20:03:14 -0300

    Add TypeScript configuration file for project setup

---
+++ infra/index.ts
@@ -0,0 +1,36 @@
+import * as digitalocean from "@pulumi/digitalocean";
+
+const app = new digitalocean.App("ziply-app", {
+  spec: {
+    name: "ziply",
+    region: "nyc3",
+    domainNames: [
+      {
+        name: "ziply.rigotti.dev",
+        type: "PRIMARY",
+      },
+    ],
+    services: [
+      {
+        name: "ziply-service",
+        github: {
+          repo: "RigottiG/ziply",
+          branch: "main",
+          deployOnPush: true,
+        },
+        instanceSizeSlug: "basic-xs",
+        instanceCount: 1,
+        httpPort: 3001,
+        envs: [
+          {
+            key: "NODE_ENV",
+            value: "production",
+          },
+        ],
+      },
+    ],
+  },
+});
+
+export const defaultAppUrl = app.defaultIngress;
+export const liveUrl = app.liveUrl;
